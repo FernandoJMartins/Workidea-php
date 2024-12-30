14b3f3a7bbb59bd17904c1a99db892c6fc9a67d8
Fer <julio8martins@gmail.com>
2024-12-30 15:13:10 -0300

    views

--- helpers.php
+++ helpers.php
@@ -4,4 +4,32 @@ function basePath($path = '') {
     return __DIR__ . '/' . $path;
 }
 
+
+function loadView($path) {
+    $viewPath = basePath("views/$path.view.php");
+
+    if (file_exists($viewPath)){
+        require $viewPath;
+    }
+    else{
+        echo "View {$path} not found";
+    }
+
+}
+
+function loadPartials($path) {
+    $partialPath = basePath("views/partials/{$path}.php");
+
+    if (file_exists($partialPath)){
+        require $partialPath;
+    }
+    else{
+        echo "Partial {$path} not found";
+    }
+
+}
+
 ?>
+
+
+
