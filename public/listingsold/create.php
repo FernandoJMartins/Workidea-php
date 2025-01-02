cd62cc0a83aae48b53d83f8498550509453b8b7b
Fer <julio8martins@gmail.com>
2025-01-02 14:31:31 -0300

    views

--- public/listingsold/create.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require "../../helpers.php";
-
-require basePath("views/listings/create.view.php")
-
-?>
-
--- public/listingsold/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require "../../helpers.php";
-
-require basePath("views/listings/index.view.php")
-
-?>
-
