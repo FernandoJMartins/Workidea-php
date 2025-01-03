f8724961935adf625859c1e82dfaf60d46b68492
Fer <julio8martins@gmail.com>
2025-01-03 01:58:30 -0300

    autoloader

--- App/views/listings/show.view.php
+++ App/views/listings/show.view.php
@@ -6,7 +6,7 @@
 <section class="container mx-auto p-4 mt-4">
       <div class="rounded-lg shadow-md bg-white p-3">
        <div class="flex justify-between items-center">
-      <a class="block p-4 text-blue-700" href="/listings">
+      <a class="block p-4 text-blue-700" href="/public/listings/">
         <i class="fa fa-arrow-alt-circle-left"></i>
         Back To Listings
       </a>
