8223263e55bb0c276db356fbd01e83015cb2087f
Fer <julio8martins@gmail.com>
2025-01-07 23:37:33 -0300

    validation

---
+++ Framework/Validation.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Framework;
+
+class Validation {
+
+    /**
+     * Validate a string
+     * 
+     * @param string $value
+     * @param int $min
+     * @param int $max
+     * @return boolean
+     */
+    public static function string($value, $min = 1, $max = INF) {
+        if (is_string($value)){
+            $value = trim($value);
+            $length = strlen($value);
+            
+            return $length >= $min && $length <= $max;
+        }
+    }
+
+    /**
+     * Validate Email Adress
+     *
+     * @param string $value
+     * @return mixed
+     */
+    public static function email($value){
+        $value = trim($value);
+
+        return filter_var($value, FILTER_VALIDATE_URL);
+    }
+
+    /**
+     * Match a value against another
+     *
+     * @param string $value
+     * @param string $value2
+     * @return bool
+     */
+    public static function match($value, $value2){
+        $value = trim($value);
+        $value2 = trim($value2);
+
+        return $value === $value2;
+    }
+}
