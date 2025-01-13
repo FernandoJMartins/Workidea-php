0d258dcd1a70192d51f7b8caae7cf8c08c59999b
Fer <julio8martins@gmail.com>
2025-01-13 12:53:04 -0300

    user controller

--- routes.php
+++ routes.php
@@ -10,3 +10,6 @@ $router -> post ('/public/listings/', 'ListingController@store');
 $router -> put ('/public/listings/{id}', 'ListingController@update');
 
 $router -> delete ('/public/listings/{id}', 'ListingController@delete');
+
+$router -> get('/public/auth/register/', 'UserController@create');
+$router -> get('/public/auth/login/', 'UserController@login');
