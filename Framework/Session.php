76077627d4b480c479dfad91c513164cbe7350db
Fer <julio8martins@gmail.com>
2025-01-13 22:18:11 -0300

    session class

---
+++ Framework/Session.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Framework;
+
+class Session {
+    //Start session
+    public static function start(){
+        if(session_status() == PHP_SESSION_NONE){
+            session_start();
+        }
+    }
+
+    //Set a session key/value pair
+    public static function set($key, $value){
+        $_SESSION[$key] = $value;
+    }
+
+    //Get a session value by the key
+    public static function get($key, $default = null){
+        return isset($_SESSION[$key]) ? $_SESSION[$key] : $default;
+    }
+
+    //Check if session key exists
+    public static function has($key){
+        return isset($_SESSION[$key]);
+    }
+
+    //Clear session by key
+    public static function clear($key){
+        if (isset($_SESSION[$key])){
+            unset($_SESSION[$key]);
+        }
+    }
+
+    //Clear all session data
+    public static function clearAll(){
+        session_unset();
+        session_destroy();
+    }
+}
